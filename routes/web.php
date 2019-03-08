1a7e83bab436c75acab778048228549dccc2a648
shojaeix <shojaei.x@gmail.com>
2019-03-08 23:31:05 +0330

    add exp.intro route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/exp', 'Exp\IntroController@create')->name('exp.intro');
