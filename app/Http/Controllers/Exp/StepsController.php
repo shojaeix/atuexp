8e55e348b40f6a213f245f37aa0f33f8fe42f6f6
shojaeix <shojaei.x@gmail.com>
2019-03-15 09:49:33 +0330

    translate steps pages errors and alerts

--- app/Http/Controllers/Exp/StepsController.php
+++ app/Http/Controllers/Exp/StepsController.php
@@ -57,7 +57,7 @@ class StepsController extends Controller
         // redirect user to result page, if Exp completed
         if($exp->last_complete_step == 4){
             // add success alert
-            session('success', 'پاسخ های شما با موفقیت ثبت شدند.');
+            session('success', 'تمام پاسخ های شما با موفقیت ثبت شدند و آزمون به پایان رسید.');
             // redirect
             return redirect(route('exp.result'));
         }
@@ -71,10 +71,31 @@ class StepsController extends Controller
     }
 
     private function validateRequest(Request $request, $number){
+
+        $requiredQuestionError =  'پاسخ دهی به :attribute الزامیست.';
+        $customErrorTexts = [
+            'question_1.required' => $requiredQuestionError,
+            'question_2.required' => $requiredQuestionError,
+            'question_3.required' => $requiredQuestionError,
+            'question_4.required' => $requiredQuestionError,
+            'question_5.required' => $requiredQuestionError,
+            'question_6.required' => $requiredQuestionError,
+        ];
+        $translateFields = [
+            'question_1' => 'سوال اول',
+            'question_2' => 'سوال دوم',
+            'question_3' => 'سوال سوم',
+            'question_4' => 'سوال چهارم',
+            'question_5' => 'سوال پنجم',
+            'question_6' => 'سوال ششم',
+            'question_7' => 'سوال هفتم',
+            'question_8' => 'سوال هشتم',
+        ];
         switch($number){
             case 1:
             case 2:
             case 3:
+                $customErrorTexts['question_6.gte'] = 'مقدار وارد شده برای :attribute باید بزرگ تر از :value باشد.';
                 return $request->validate([
                     'question_1' => 'required|int|min:-4|max:4',
                     'question_2' => 'required|int|min:1|max:4',
@@ -85,7 +106,7 @@ class StepsController extends Controller
                     'question_5' => 'required|int',
                     'question_6' => 'required|int|gte:question_5',
 
-                ]);
+                ], $customErrorTexts, $translateFields);
                 break;
             case 4:
                 return $request->validate([
@@ -100,7 +121,7 @@ class StepsController extends Controller
 
                     'question_7' => 'required|string|in:yes,no',
                     'question_8' => 'required|string|in:a,b,c,d',
-                ]);
+                ], $customErrorTexts, $translateFields);
                 break;
             default: abort(404);
         }
