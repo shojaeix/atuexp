9d3040f31593879495659c30ac7367e0c4e28a18
shojaeix <shojaei.x@gmail.com>
2019-03-19 19:08:20 +0330

    show website title on middle of welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    آزمون مطلاعاتی دانشگاه طباطبایی
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
