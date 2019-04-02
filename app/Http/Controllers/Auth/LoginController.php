d9e05c218cfc3393e4c753ee26dd20f3cc9883a6
shojaeix <shojaei.x@gmail.com>
2019-04-02 09:37:25 +0430

    redirect to exp.management page after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/exp/management';
 
     /**
      * Create a new controller instance.
